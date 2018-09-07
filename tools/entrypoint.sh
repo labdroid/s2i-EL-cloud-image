7fe8cb1d4fe627d676b9558bbfa91a99801c933b
Anthony Green <green@moxielogic.com>
2018-09-07 15:32:38 -0400

    Update

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -1,11 +1,20 @@
+#!/bin/sh
 
-IMAGE_NAME = rhel7
+set -x
 
-.PHONY: build
-build:
-	docker build -t $(IMAGE_NAME) .
+if [ -z "$VAULT_MINIO_READ_ACCESS_TOKEN" ]; then
+    echo ERROR: VAULT_MINIO_READ_ACCESS_TOKEN environment variable missing.  Create this secret in OpenShift.
+    exit 1
+fi
 
-.PHONY: test
-test:
-	docker build -t $(IMAGE_NAME)-candidate .
-	IMAGE_NAME=$(IMAGE_NAME)-candidate test/run
+SECRETS=`curl -H "X-Vault-Token: $VAULT_MINIO_READ_ACCESS_TOKEN" -X GET http://vault-labdroid.apps.home.labdroid.newt:8200/v1/secret/minio`
+
+MINIO_ACCESS_KEY=`echo $SECRETS | jq -r .data.MINIO_ACCESS_KEY`
+MINIO_SECRET_KEY=`echo $SECRETS | jq -r .data.MINIO_SECRET_KEY`
+
+echo export MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY > minio.env
+echo export MINIO_SECRET_KEY=$MINIO_SECRET_KEY >> minio.env
+
+while true; do
+    sleep 1000;
+done;
